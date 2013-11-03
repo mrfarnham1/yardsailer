beafae139ba53045935f622e9e3d651183d9c58f
Joe Heald <joeheald85@gmail.com>
2013-11-03 01:49:22 -0400

    init framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Yardsailer::Application.config.secret_key_base = '4ae68885be1a668e6a422d5f53d1e8f08d4eec7d4c24043634f56520011ec48e1f11388df2a255a6486ecbe9ab7a6746c06f323e64380042cc2226ba51f9aacb'
